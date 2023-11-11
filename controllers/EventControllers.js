de0df81a65d22c1150477eabe7381dbc855bda24
yuchenbao <yuchenbao@chenbaoMBP.lan>
2023-11-11 01:00:27 +0100

    cookie

--- controllers/EventControllers.js
+++ controllers/EventControllers.js
@@ -13,12 +13,12 @@ module.exports.checkLoginInformations = async (req, res, next) => {
     const {userName, userCode } = body;
     const user = await User.find({userName: userName});
     if(userCode === user[0].userCode) {
-        // res.cookie('test', '1200', {
-        //     maxAge: 60 * 60 * 24 * 1000, // 设置Cookie的过期时间，单位为毫秒
-        //     httpOnly: true, // 将Cookie设置为只能通过HTTP协议访问
-        //     secure: true, // 仅在HTTPS连接中传输Cookie
+        res.cookie('test', '1200', {
+            maxAge: 60 * 60 * 24 * 1000, // 设置Cookie的过期时间，单位为毫秒
+            httpOnly: true, // 将Cookie设置为只能通过HTTP协议访问
+            secure: true, // 仅在HTTPS连接中传输Cookie
             sameSite: 'none', // 允许跨站点访问
-        // });
+        });
         req.session.user = body;
         req.session.test2 = 123456;
         console.log('req.session 2= ', req.session);
